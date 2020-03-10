2675c5f368a7ba5e91bccc44ed8c64a9264cf18f
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-10 14:14:34 -0400

    Added api_key and jwt token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,53 @@
 class ApplicationController < ActionController::API
+
+    before_action :authorized
+
+    def encode_token(payload)
+
+        #note: temporary. Change secret in secret base
+        JWT.encode(payload, Rails.application.credentials.project[:jwt])
+
+        #payload => {beef: 'steak'}
+        #jwt_string:
+        #"eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI"
+    end 
+
+    def auth_header 
+        # {Authorization: 'Bearer <token'}
+        request.headers['Authorization']
+    end 
+
+    def decoded_token
+
+        if auth_header
+            token = auth_header.split(' ')[1]
+            #headers: {'Authorization': 'Bearer <token>'}
+            begin
+                #change secret here to environment variable
+                JWT.decode(token, Rails.application.credentials.project[:jwt], true, algorithm: 'HS256')
+             # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
+            rescue JWT::DecodeError
+                nil
+            end 
+        end
+    end 
+
+    def current_user
+
+        if decoded_token
+            #decoded_token => [{"user_id" => 2}, {"alg" => "HS256"}]
+            #or nil if cannot be decoded
+
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end 
+    end 
+
+    def logged_in?
+        !!current_user
+    end 
+
+    def authorized
+        render json: {message: 'Please log in'}, status: :unauthorized unless logged_in?
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/create'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :users, only: [:create, :index]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end 
 end
