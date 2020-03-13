7837b3ef7375d2f470d45b7c29994efabccd66ae
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-13 15:46:44 -0400

    Login profile and collection rendering working

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -10,7 +10,34 @@ class Api::V1::AuthController < ApplicationController
     if @user && @user.authenticate(user_login_params[:password])
       # encode token comes from ApplicationController
       token = encode_token({ user_id: @user.id })
-      render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+
+      #gets the collection in addition to the user profile 
+      user_collection = Collection.all.select{|collection| collection.user == @user}
+        # user_collection = user_collection.map{|collection| collection.game}
+
+        new_collection = []
+
+        user_collection.each do |collection|
+
+            new_obj = {}
+            game = collection.game 
+
+            new_obj["game_id"] = game.game_id 
+            new_obj["name"] = game.name 
+            new_obj["year_published"] = game.year_published
+            new_obj["min_players"] = game.min_players
+            new_obj["max_players"] = game.max_players 
+            new_obj["description"] = game.description 
+            new_obj["image_url"] = game.image_url 
+            new_obj["max_playtime"] = game.max_playtime
+            new_obj["min_playtime"] = game.min_playtime  
+            new_obj["favorite"] = collection.favorite
+            new_obj["owned"] = collection.owned
+
+            new_collection << new_obj
+        end 
+
+      render json: { user: UserSerializer.new(@user), jwt: token, user_collection: new_collection }, status: :accepted
     else
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
