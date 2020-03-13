c47a1ab3a92ffd964e3e0c9675911264ebe7a00c
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-13 13:10:40 -0400

    Finished users/collection/:id api that includes favorite/owned status

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
-    skip_before_action :authorized, only: [:create, :index] #remove index later
+    skip_before_action :authorized, only: [:create, :index, :user_collection] #remove index and user_collection for production
 
     def profile
         render json: {user: UserSerializer.new(current_user)}, status: :accepted
@@ -22,6 +22,39 @@ class Api::V1::UsersController < ApplicationController
 
         users = User.all 
         render json: users 
+    end 
+
+    #used for db searching, remove from routes when needed
+    def user_collection
+
+        user = User.find_by(id: params[:id])
+        user_collection = Collection.all.select{|collection| collection.user == user}
+        # user_collection = user_collection.map{|collection| collection.game}
+
+        new_collection = []
+
+        user_collection.each do |collection|
+
+            new_obj = {}
+            game = collection.game 
+
+            new_obj["game_id"] = game.game_id 
+            new_obj["name"] = game.name 
+            new_obj["year_published"] = game.year_published
+            new_obj["min_players"] = game.min_players
+            new_obj["max_players"] = game.max_players 
+            new_obj["description"] = game.description 
+            new_obj["image_url"] = game.image_url 
+            new_obj["max_playtime"] = game.max_playtime
+            new_obj["min_playtime"] = game.min_playtime  
+            new_obj["favorite"] = collection.favorite
+            new_obj["owned"] = collection.owned
+
+            new_collection << new_obj
+        end 
+
+        #renders a user id's collection and includes favorite/owned status 
+        render json: new_collection
     end 
 
     private 
