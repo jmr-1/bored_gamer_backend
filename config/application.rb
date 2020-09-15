acadc58e52deb8be3099c02744b80c7d6084bf82
John Louis Rumingan <jmrumingan@gmail.com>
2020-09-14 20:41:36 -0400

    Attempt cors fix

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,11 @@ module BoredGameBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+        resource(
+          '*', 
+          headers: :any, 
+          methods: [:get, :post, :patch, :options]
+        )
       end
     end
 
