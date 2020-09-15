41405be39ad74eba1da37293c3916884630b808c
John Louis Rumingan <55402017+JohnLouisRumingan@users.noreply.github.com>
2020-09-15 15:56:42 -0400

    Use development version of ruby for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,8 @@ module BoredGameBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    Rails.application.config.hosts << "bored-gamer-meetup.herokuapp.com/"
+
     # config.middleware.insert_before 0, Rack::Cors do
     #   allow do
     #     origins '*'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bored-gamer-meetup.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001'
-    # origins '*'
+    origins ['https://bored-gamer-meetup.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
