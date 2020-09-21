25ce576d511b0584c3ef0c99d0f904ed51595f3d
John-Louis Rumingan <55402017+JohnLouisRumingan@users.noreply.github.com>
2020-09-21 18:54:54 -0400

    Modify meetups controller add/remove user to only render a single object in json

--- app/controllers/api/v1/meetups_controller.rb
+++ app/controllers/api/v1/meetups_controller.rb
@@ -85,27 +85,29 @@ class Api::V1::MeetupsController < ApplicationController
         #     meetup.users.delete(user)
         # end 
 
-        meetups = Meetup.all 
-        detailed_array = []
-        meetups.each do |meetup|
-            newObj = {}
-            newObj["meetup_details"] = meetup
-            newObj["host"] = meetup.user
-            newObj["participants"] = meetup.users
+        # meetups = Meetup.all 
+        # detailed_array = []
+        # meetups.each do |meetup|
+        #     newObj = {}
+        #     newObj["meetup_details"] = meetup
+        #     newObj["host"] = meetup.user
+        #     newObj["participants"] = meetup.users
             
-            meetup_collection = []
-            meetup.collections.each do |collection|
-                collObj = {}
-                collObj["owner"] = collection.user
-                collObj["game"] = collection.game
-                meetup_collection << collObj
-            end 
-            newObj["collection"] = meetup_collection
-            detailed_array << newObj
-        end 
+        #     meetup_collection = []
+        #     meetup.collections.each do |collection|
+        #         collObj = {}
+        #         collObj["owner"] = collection.user
+        #         collObj["game"] = collection.game
+        #         meetup_collection << collObj
+        #     end 
+        #     newObj["collection"] = meetup_collection
+        #     detailed_array << newObj
+        # end 
+
+        # render json: detailed_array
 
         # why return the entire array? Just return the modified object like below
-        render json: detailed_array
+        render json: {meetup_details: meetup, host: meetup.user, participants: meetup.users, collection: meetup_collections} 
     end 
 
 
